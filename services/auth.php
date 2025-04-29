9fb939a96c60c00614dcc179c3827bbabf134eb3
julia-daniluski <julia.o.daniluski@gmail.com>
2025-04-29 09:13:21 -0300

    Update auth.php

---
+++ services/auth.php
@@ -0,0 +1,34 @@
+<?php
+// define espaço para a organização do código
+namespace services;
+class Auth{
+    private array $usuarios = [];
+
+    // Método construtor
+    public function __construct(){
+        $this->carregarUsuarios();
+    }
+    // metodo para carregar usuarios do arquivo json
+    private function carregarUsuarios(): void{
+        // verificar se existe o arquivo
+        if(file_exists(ARQUIVO_USUARIOS)){
+            // Le o conteufo e decodifics o json para o array
+            $conteudo = json_decode(file_get_contents(ARQUIVO_USUARIOS),true);
+
+            // verificar se é um array
+            $this->usuarios = is_array($conteudo) ? $conteudo : [];
+        }
+
+        else {
+            $this -> usuarios =[
+                ['username' => 'adm',
+                 'password' => password_hash('adm123', PASSWORD_DEFAULT),
+                 'perfil' => 'adm'],
+            [   'username' => 'usuario',
+                'password' => password_hash('usuario123', PASSWORD_DEFAULT),
+                'perfil' => 'usuario']
+            ];
+        }
+    }
+}
+?>
